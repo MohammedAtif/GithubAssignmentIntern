760bf394a608193eae8ead50edaea9611f7a8c67
sreejakamineni <sreeja.kamineni@zemosolabs.com>
2020-03-11 11:02:35 +0530

    test file updated

--- src/test/java/com/zemoso/githubtask/assignment/services/CalculationServiceTest.java
+++ src/test/java/com/zemoso/githubtask/assignment/services/CalculationServiceTest.java
@@ -10,7 +10,7 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class CalculationServiceTest {
 
-    private CalculationService calculationService;
+ private CalculationService calculationService;
 
     @Before
     public void setup(){
@@ -19,26 +19,30 @@ public class CalculationServiceTest {
 
     @Test
     public void testResultInt(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateInt(query));
+        Assert.assertEquals(6, calculationService.calculateInt("2*3"));
+        Assert.assertEquals(-3, calculationService.calculateInt("1*(2+3)-8"));
+        Assert.assertEquals(25, calculationService.calculateInt("1+3*8"));
     }
 
     @Test
     public void testResultFloat(){
-        String query = "";
-        Assert.assertEquals(0f, calculationService.calculateFloat(query), 0f);
+        Assert.assertEquals(7.0, calculationService.calculateFloat("2+2.5*2"), 0f);
+        Assert.assertEquals(7.5, calculationService.calculateFloat("3*2.5"), 0f);
+        Assert.assertEquals(20.5, calculationService.calculateFloat("6+4+10.5"), 0f);
     }
 
     @Test
     public void testResultLong(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateLong(query));
+			Assert.assertEquals(Long.parseLong("200000000000"), calculationService.calculateLong("1000000*2000000/10"));
+			Assert.assertEquals(Long.parseLong("987654330086"), calculationService.calculateLong("987654321077+9009"));
+ 
     }
 
     @Test
     public void testResultDouble(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateDouble(query), 0);
+    Assert.assertEquals(5.2, calculationService.calculateDouble("(5.38-4.34)*5"), 0);
+    Assert.assertEquals( 581216724.4865831, calculationService.calculateDouble("581216732.323433-7.83684987683688"), 0);
+       
     }
 
-}
+	}
