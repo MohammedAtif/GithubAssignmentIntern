835d45853d9f79c0f16eaba6c09898dbcdb68c18
madhuri <madhuri.kotha@zemosolabs.com>
2020-03-11 12:13:37 +0530

    Added test cases

--- src/test/java/com/zemoso/githubtask/assignment/services/CalculationServiceTest.java
+++ src/test/java/com/zemoso/githubtask/assignment/services/CalculationServiceTest.java
@@ -19,26 +19,89 @@ public class CalculationServiceTest {
 
     @Test
     public void testResultInt(){
-        String query = "";
+
+        String query = "";//expression can't be null
         Assert.assertEquals(0, calculationService.calculateInt(query));
+
+        query = "8888";
+        Assert.assertEquals(8888, calculationService.calculateInt(query));
+
+        query="1*2+(3*4+(30/5-5+(4/2)*12-1)+1)-20";
+        Assert.assertEquals(19,calculationService.calculateInt(query));
+
+        query="2-(2)";
+        Assert.assertEquals(0,calculationService.calculateInt(query));
+
+        query="2(4--3)";//missing operator
+        Assert.assertEquals(0,calculationService.calculateInt(query));
+
+        query="1**2";//invalid sign
+        Assert.assertEquals(0,calculationService.calculateInt(query));
+
+        query="1*2*";//invalid expression
+        Assert.assertEquals(0,calculationService.calculateInt(query));
+
+        query="*1*2";//invalid expression
+        Assert.assertEquals(0,calculationService.calculateInt(query));
+
     }
 
     @Test
     public void testResultFloat(){
-        String query = "";
+
+        String query = "";//expression can't be null
         Assert.assertEquals(0f, calculationService.calculateFloat(query), 0f);
+
+        query="(90.99--9.01)*10/0";//number can't be divided by 0
+        Assert.assertEquals(0f, calculationService.calculateFloat(query), 0f);
+
+        query="{[({9+0.9}-0.9)*-100.01]-2}";
+        Assert.assertEquals(-902.09f, calculationService.calculateFloat(query), 0f);
+
+        query="{[({9+0.9}-0.9)*-100.01]-2f}";//alphabets are not allowed
+        Assert.assertEquals(0f, calculationService.calculateFloat(query), 0f);
+
     }
 
     @Test
     public void testResultLong(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateLong(query));
+
+        String query = "";//expression can't be null
+        Assert.assertEquals(0L, calculationService.calculateLong(query));
+
+        query = "+";//invalid expression
+        Assert.assertEquals(0L, calculationService.calculateLong(query));
+
+        query = "(+)";//invalid expression
+        Assert.assertEquals(0L, calculationService.calculateLong(query));
+
+        query = "32434+23232-343432*2323";
+        Assert.assertEquals(-797736870L, calculationService.calculateLong(query));
+
+        query = "32434+23232-(343432*2323)t";//alphabets are not allowed in an expression
+        Assert.assertEquals(0L, calculationService.calculateLong(query));
     }
 
     @Test
     public void testResultDouble(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateDouble(query), 0);
-    }
 
+        String query = "";//expression can't be null
+        Assert.assertEquals(0, calculationService.calculateDouble(query), 0);
+
+        query = "(1223.231)";
+        Assert.assertEquals(1223.231, calculationService.calculateDouble(query), 0);
+
+        query = "32434.129+23232.12309-343432.2382/(343432.2382*2)";
+        Assert.assertEquals(55665.75209, calculationService.calculateDouble(query),0.0);
+
+        query = "(-34.9034)+23.232";
+        Assert.assertEquals(-11.6714, calculationService.calculateDouble(query),0.00001);
+
+        query="{[({9+0.9}-0.9)*-100)-2}";//brackets are not closed properly
+        Assert.assertEquals(0.0, calculationService.calculateDouble(query),0.00001);
+
+        query="1.2null+1";//alphabets are not allowed in an expression
+        Assert.assertEquals(0.0, calculationService.calculateDouble(query),0.00001);
+
+    }
 }
