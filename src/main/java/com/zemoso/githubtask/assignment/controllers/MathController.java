3ae8ec30bd0959570c798ffef60d2dbc9b6f1fa4
fareedsheik <sheikfareed3819@gmail.com>
2020-03-11 11:08:38 +0530

    Tests and URL mappings are added

--- src/main/java/com/zemoso/githubtask/assignment/controllers/MathController.java
+++ src/main/java/com/zemoso/githubtask/assignment/controllers/MathController.java
@@ -2,23 +2,71 @@ package com.zemoso.githubtask.assignment.controllers;
 
 import com.zemoso.githubtask.assignment.pojos.MathResponse;
 import com.zemoso.githubtask.assignment.services.CalculationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("calculate")
 public class MathController {
 
+    MathResponse mathResponse = new MathResponse();
     private final CalculationService calculationService;
 
+    @Autowired
     public MathController(CalculationService calculationService) {
         this.calculationService = calculationService;
     }
 
     @GetMapping("double")
-    public @ResponseBody MathResponse calculateQuery(@RequestParam(required = false) String query){
-        MathResponse mathResponse = new MathResponse();
-        mathResponse.setValueDouble(calculationService.calculateDouble(query));
+    public @ResponseBody
+    MathResponse calculateQuery(@RequestParam(required = false) String query) {
+        try {
+            mathResponse.setValueDouble(calculationService.calculateDouble(query));
+        } catch (Exception e) {
+
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "WRONG INPUT", e);
+        }
         return mathResponse;
     }
 
+    @GetMapping("int")
+    public @ResponseBody
+    MathResponse calculateQueryInt(@RequestParam(required = false) String query) {
+        try{
+            mathResponse.setValueInt(calculationService.calculateInt(query));}
+        catch (Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "WRONG INPUT", e);
+
+        }
+        return mathResponse;
+    }
+
+    @GetMapping("float")
+    public @ResponseBody
+    MathResponse calculateQueryFloat(@RequestParam(required = false) String query) {
+        try{
+            mathResponse.setValueFloat(calculationService.calculateFloat(query));}
+        catch (Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "WRONG INPUT", e);
+
+        }
+        return mathResponse;
+    }
+
+    @GetMapping("long")
+    public @ResponseBody
+    MathResponse calculateQueryLong(@RequestParam(required = false) String query) {
+        try{
+            mathResponse.setValueLong(calculationService.calculateLong(query));}
+        catch (Exception e)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "WRONG INPUT", e);
+
+        }
+        return mathResponse;
+    }
 }
--- src/test/java/com/zemoso/githubtask/assignment/services/CalculationServiceTest.java
+++ src/test/java/com/zemoso/githubtask/assignment/services/CalculationServiceTest.java
@@ -1,6 +1,7 @@
 package com.zemoso.githubtask.assignment.services;
 
 import com.zemoso.githubtask.assignment.services.impl.PostfixCalculationService;
+import org.apache.catalina.connector.Response;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,36 +10,84 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class CalculationServiceTest {
-
     private CalculationService calculationService;
 
     @Before
-    public void setup(){
+    public void setup() {
         calculationService = new PostfixCalculationService();
     }
 
     @Test
-    public void testResultInt(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateInt(query));
+    public void testResultInt() {
+        Assert.assertEquals("Testing addition", 6, calculationService.calculateInt("2+2+2"));
+        Assert.assertEquals("Testing subtraction", 25, calculationService.calculateInt("24-2+3"));
+        Assert.assertEquals("Testing multiplication", 8, calculationService.calculateInt("2+2*3"));
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testResultIntFormatException() {
+        calculationService.calculateInt("2+2a+3");
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testResultIntArithematicException() {
+        calculationService.calculateInt("2+2+2/0");
     }
 
     @Test
-    public void testResultFloat(){
-        String query = "";
-        Assert.assertEquals(0f, calculationService.calculateFloat(query), 0f);
+    public void testResultFloat() {
+        Assert.assertEquals("Testing addition", 7.5, calculationService.calculateFloat("2.5+2.5+2.5"), 0f);
+        Assert.assertEquals("Testing subtraction", 20.9, calculationService.calculateFloat("24.9-7+3"), 1f);
+        Assert.assertEquals("Testing multiplication", 9.8, calculationService.calculateFloat("2+2*3.9"), 1f);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testResultFloatFortmatException() {
+        calculationService.calculateInt("2.0o+2a+3.8");
+        calculationService.calculateInt("2.0+2a+3.8''");
+        calculationService.calculateInt("2.0+2a+3.8.,");
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testResultFloatArithematicException() {
+        calculationService.calculateFloat("2.0+2.8/0");
     }
 
     @Test
-    public void testResultLong(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateLong(query));
+    public void testResultDouble() {
+        Assert.assertEquals("Testing addition", 7.746700000000001, calculationService.calculateDouble("2.5767+2.59+2.58"), 0f);
+        Assert.assertEquals("Testing subtraction", 5677.98, calculationService.calculateDouble("2498.98-798+3977"), 0f);
+        Assert.assertEquals("Testing multiplication", 3843.3999999999996, calculationService.calculateDouble("2767+276*3.9"), 0f);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testResultDoubleFormatException() {
+        calculationService.calculateInt("2.878780o+2a+3.8");
+        calculationService.calculateInt("2.067+2a+3.8''");
+        calculationService.calculateInt("2.0+2a+3.887678.,");
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testResultDoubleArithematicException() {
+        calculationService.calculateFloat("28787.0989+2.8767/0");
     }
 
     @Test
-    public void testResultDouble(){
-        String query = "";
-        Assert.assertEquals(0, calculationService.calculateDouble(query), 0);
+    public void testResultLong() {
+        Assert.assertEquals("Testing addition", 3.8681464904E10, calculationService.calculateDouble("38678676767+2787879+258"), 0);
+        Assert.assertEquals("Testing subtraction", 2.49879977E8, calculationService.calculateDouble("249876798-798+3977"), 0);
+        Assert.assertEquals("Testing multiplication", 13531, calculationService.calculateDouble("2767+276*39"), 0);
     }
 
+    @Test(expected = NumberFormatException.class)
+    public void testResultLongFormatException() {
+        calculationService.calculateInt("2878780o+2a+38");
+        calculationService.calculateInt("2067+2a+38''");
+        calculationService.calculateInt("20+2a+3887678.,");
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testResultLongArithematicException() {
+        calculationService.calculateFloat("287870989+28767/0");
+    }
 }
